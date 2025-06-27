e74398ab50bbc4cf1eff4dbde046b8d6f593bdba
fdelgadodyvenia <fdelgado@dyvenia.com>
2025-06-27 15:54:46 +0200

    fix

--- crm_project/crm_app/forms.py
+++ crm_project/crm_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class ClientForm(forms.Form):
     """
-    peonfpwenfpnfpeoafpewofie2bodoqndoinoiqndoiqndonqioqdqowin
+    peonfpwenfpnfpeoafpewofie2bodoqndoinoiqndoiqndonqioqdqowin.
     """
     name = forms.CharField(label='Name', max_length=100)
     surname = forms.CharField(label='Surname', max_length=100)
