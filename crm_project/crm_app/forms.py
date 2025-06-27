c2029372e00973d55c62895bca9adcbe0581a6a2
fdelgadodyvenia <fdelgado@dyvenia.com>
2025-06-27 15:51:19 +0200

    update forms

--- crm_project/crm_app/forms.py
+++ crm_project/crm_app/forms.py
@@ -1,8 +1,12 @@
-from django import forms
-"""oefiefoenfeowinfeowinfewoinfewonfoewinfoiewnfoiewnfoewnfoewnfoewnfeowinfion
-tgrggrtgrtgtrgoioeiwndoiendoiendoadoqdno
-"""
+from django import (
+    forms,
+)
+
+
 class ClientForm(forms.Form):
+    """oefiefoenfeowinfeowinfewoinfewonfoewinfoiewnfoiewnfoewnfoewnfoewnfeowinfion
+    tgrggrtgrtgtrgoioeiwndoiendoiendoadoqdno
+    """
     name = forms.CharField(label='Name', max_length=100)
     surname = forms.CharField(label='Surname', max_length=100)
     email = forms.CharField(label='Email', max_length=100)
